dc85c32e16c1544eb00895277f8e930585861cab
KarlangaXZ <carlos.josewin@gmail.com>
2025-07-22 16:01:24 -0400

    First commit to git repository

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,96 @@
+import {  Code, Earth, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">Passionate Web Developer</h3>
+
+            <p className="text-muted-foreground">
+              I'm a passionate frontend developer with a strong foundation in
+              HTML, CSS, and JavaScript. I enjoy creating responsive and
+              user-friendly websites that provide great user experiences.
+            </p>
+
+            <p className="text-muted-foreground">
+              My web development journey began in 2022 after I decided to
+              transition from a sales and accounting career to a web developer.
+              Since then, I've been constantly learning and improving my skills
+              to stay up-to-date with the latest technologies and best
+              practices.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="https://drive.google.com/file/d/1SljJ88sYHqRCqdTJcuwf81D58S70KHKG/view?usp=drive_link"
+                target="_blank"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">
+                    {" "}
+                    Fullstack Developer
+                  </h4>
+                  <p className="text-muted-foreground">
+                    Creating modern, responsive websites with a focus on
+                    performance and accessibility.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Earth className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Web Development</h4>
+                  <p className="text-muted-foreground">
+                    Design for businesses and organizations with wordpress.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Customer Service</h4>
+                  <p className="text-muted-foreground">
+                    Support with a focus on user satisfaction and engagement.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
