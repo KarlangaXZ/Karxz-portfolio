399388355e59e54766ce1bd87cbd9ce79bf8d1e7
KarlangaXZ <carlos.josewin@gmail.com>
2025-09-03 21:58:12 -0400

    a ver

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./public/projects/CV.pdf"
+                href="https://github.com/KarlangaXZ/Karxz-portfolio/blob/69ccdcd1d9b473362e5b5f40fe294d1917ec3087/public/projects/CV.pdf"
                 download="CV.pdf"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
